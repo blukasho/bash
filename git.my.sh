f57090e6ae68eff53c8c85ff6df20781b6dce4a8
blukasho <bodik1w@gmail.com>
2019-07-30 13:51:30 +0300

    fix line

--- git.my.sh
+++ git.my.sh
@@ -12,8 +12,8 @@ read -p "Do you want commit and push changes? y || n " answer
 if [ $answer = $yes ]
 then
 	echo -n "Add commit desctiption: "
-	readline -e answer
-	git commit -m $answer
+	read answer
+	git commit -m "$answer"
 	git push origin master
 fi
 
